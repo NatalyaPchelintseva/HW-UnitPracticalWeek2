d2e5d43cabaae7ab6b0dc7760a4f75fad8c3fb33
Natalya Pchelintseva <pchelintseva.n@yandex.ru>
2022-02-19 21:47:29 +0300

    class Horse is finished.

--- src/Horse.java
+++ src/Horse.java
@@ -9,12 +9,26 @@ public class Horse extends ChessPiece {
     }
 
     @Override
-    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+    public boolean canMoveToPosition(ChessBoard chessBoard,
+                                     int line, int column, int toLine, int toColumn) {
+        if (line != toLine && column != toColumn) {
+            if (checkPosition(toLine) && checkPosition(toColumn)) {
+                int dx = Math.abs(toLine - line);
+                int dy = Math.abs(toColumn - column);
+                if ((dx == 1 && dy == 2) || (dx == 2 && dy == 1)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public String getSymbol() {
         return "H";
+    }
+
+    private boolean checkPosition(int position) {
+        return ((position >= 0) && (position <= 7));
     }
 }
