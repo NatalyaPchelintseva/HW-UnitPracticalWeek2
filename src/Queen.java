3180569088ae513ece4c39fda3e0d6d0a1a1d0aa
Natalya Pchelintseva <pchelintseva.n@yandex.ru>
2022-02-22 16:01:32 +0300

    Queen are corrected

--- src/Queen.java
+++ src/Queen.java
@@ -11,14 +11,33 @@ public class Queen extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        boolean isInSingleCell = false;
+        boolean isInStraightLine = false;
+        boolean isInDiagonal = false;
+        int[] leftUpCorner = new int[2];
+        leftUpCorner[0] = line + 1;
+        leftUpCorner[1] = column - 1;
+        int[] leftDownCorner = new int[2];
+        leftDownCorner[0] =  line - 1;
+        leftDownCorner[1] = column - 1;
+        int[] rightUpCorner = new int[2];
+        rightUpCorner[0] = line + 1;
+        rightUpCorner[1] = column + 1;
+        int[] rightDownCorner = new int[2];
+        rightDownCorner[0] = line - 1;
+        rightDownCorner[1] = line + 1;
+
         if (checkPosition(line) && checkPosition(column) && checkPosition(toLine) && checkPosition(toColumn)) {
-            // if (Math.abs(line - toLine) > 1 || Math.abs(column - toColumn) > 1) return false;
-            if (isUnderAttack(chessBoard, toLine, toColumn)) return false;
-            if (chessBoard.board[toLine][toColumn] != null) {
-                return !chessBoard.board[toLine][toColumn].getColor().equals(this.color);
-            }
-            return true;
-        } else return false;
+            if (Math.abs(line - toLine) == 1 || Math.abs(column - toColumn) == 1) isInSingleCell = true;
+            if ((line == toLine) || (column == toColumn)) isInStraightLine = true;
+            if ((toLine == leftUpCorner[0] && toColumn == leftUpCorner[1]) ||
+                    (toLine == leftDownCorner[0] && toColumn == leftDownCorner[1]) ||
+                    (toLine == rightUpCorner[0] && toColumn == rightUpCorner[1]) ||
+                    (toLine == rightDownCorner[0] && toColumn == rightDownCorner[1]))
+                isInDiagonal = true;
+            if (isInDiagonal || isInStraightLine || isInSingleCell) return true;
+        }
+        return false;
     }
 
     @Override
